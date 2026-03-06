be5c19b4fb04f5f775d3f361a0f95b876a7904ad
luktho31 <luktho31@gmail.com>
2026-03-06 20:27:35 +0100

    Add exercises.js

---
+++ frontend/src/pages/data/exercises.js
@@ -0,0 +1,7 @@
+const exercises = [
+  { id: 1, name: "Squat", image: "/images/squat.png", description: "Leg exercise" },
+  { id: 2, name: "Bench Press", image: "/images/bench.png", description: "Chest exercise" },
+  { id: 3, name: "Deadlift", image: "/images/deadlift.png", description: "Back exercise" },
+];
+
+export default exercises;
